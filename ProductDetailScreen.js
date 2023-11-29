f961ea56f4df2b90d1fab7b63eb46ef6decc0258
Serdar Yildiz <serdaryildizdev@gmail.com>
2023-11-29 13:45:30 +0300

    edited product detail screen

--- ProductDetailScreen.js
+++ ProductDetailScreen.js
@@ -9,8 +9,12 @@ const ProductDetailScreen = ({ route }) => {
     <View style={styles.container}>
       <Image style={styles.image} source={{ uri: item.thumbnail }} />
       <Text style={styles.title}>{item.title}</Text>
-      <Text style={styles.description}>{item.description}</Text>
-      <Text style={styles.price}>Price: ${item.price}</Text>
+      <Text style={[styles.description, { textAlign: "center" }]}>
+        {item.description}
+      </Text>
+      <Text style={[styles.price, { fontWeight: "bold", fontSize: "20" }]}>
+        Price: ${item.price}
+      </Text>
     </View>
   );
 };
@@ -19,12 +23,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "start",
+    padding: 5,
   },
   image: {
     width: 200,
     height: 200,
     marginBottom: 10,
+    marginTop: 50,
   },
   title: {
     fontSize: 24,
