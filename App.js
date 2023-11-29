07db2f67b607fc34c499f633c38a7f5abbf299a3
Serdar Yildiz <serdaryildizdev@gmail.com>
2023-11-29 11:43:17 +0300

    listed all products

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import {
+  FlatList,
+  Image,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+const BASE_URL = "https://dummyjson.com";
 
 export default function App() {
+  const [products, setproducts] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/products`)
+      .then((res) => res.json())
+      .then((data) => setproducts(data));
+    console.log(products);
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-    </View>
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>Products</Text>
+      <FlatList
+        data={products.products}
+        keyExtractor={(item) => item.id.toString()}
+        renderItem={({ item }) => (
+          <View
+            style={{
+              flexDirection: "row",
+              alignItems: "stretch",
+              justifyContent: "stretch",
+              gap: 16,
+              padding: 10,
+            }}
+          >
+            <Image
+              style={{ width: 64, height: 64, marginRight: 10 }}
+              source={{ uri: item.thumbnail }}
+            />
+            <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+              {item.title}
+            </Text>
+          </View>
+        )}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
